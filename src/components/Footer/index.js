728086a8c467de464af57a91a471a59124e6ac52
Bianca Carvalho <carvalho.bianca@hotmail.com>
2023-10-19 18:49:37 +0100

    project finalized

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,10 @@
+import { MarkGithubIcon } from "@primer/octicons-react"
 import styles from "./Footer.module.css"
 
 export default function Footer() {
     return(
         <footer className={styles.footer}>
-            <h2 className={styles.text}>Desenvolvido por Alura</h2>
+            <a className={styles.text} href="https://github.com/Bianca-L-Carvalho"><MarkGithubIcon size={23} />Bianca-L-Carvalho</a>
         </footer>
     )
 }
--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -18,6 +18,7 @@ export default function Player() {
           if (!response.ok) {
             throw new Error("Erro na API");
           }
+          
           return response.json();
         })
         .then(data => {
